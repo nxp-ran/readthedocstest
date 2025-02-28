89360dfce0242b1675fbaf9217d8e46c50fc3bbf
Ran Chen <ran.chen_1@nxp.com>
2025-02-28 17:31:26 +0800

    first commit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'readdoctest'
+copyright = '2024, Ran'
+author = 'Ran'
 
 release = '0.1'
 version = '0.1.0'
